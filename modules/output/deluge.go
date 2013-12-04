8e0f5d69f1e3502dd1e140728a50cd63fa13a7ca
Bruno Albuquerque <bga@bug-br.org.br>
2013-12-04 12:59:54 -0200

    Make DelugeOutputModule use GenericOutputModule.
    - This simplifies its code somewhat.

--- modules/output/deluge.go
+++ modules/output/deluge.go
@@ -2,7 +2,6 @@ package input
 
 import (
 	"fmt"
-	"sync"
 
 	"github.com/brunoga/go-pipeliner/datatypes"
 
@@ -12,25 +11,24 @@ import (
 )
 
 type DelugeOutputModule struct {
-	*base_modules.GenericModule
-
-	inputChannel chan *datatypes.PipelineItem
-	quitChannel  chan struct{}
+	*pipeliner_modules.GenericOutputModule
 
 	delugeClient *deluge.Deluge
 }
 
 func NewDelugeOutputModule(specificId string) *DelugeOutputModule {
-	return &DelugeOutputModule{
-		base_modules.NewGenericModule("Deluge Output Module", "1.0.0",
-			"deluge", specificId, "pipeliner-output"),
-		make(chan *datatypes.PipelineItem),
-		make(chan struct{}),
+	delugeOutputModule := &DelugeOutputModule{
+		pipeliner_modules.NewGenericOutputModule("Deluge Output Module",
+			"1.0.0", "deluge", specificId, nil),
 		nil,
 	}
+	delugeOutputModule.SetConsumerFunc(delugeOutputModule.sendItemToDeluge)
+
+	return delugeOutputModule
 }
 
-func (m *DelugeOutputModule) Configure(params *base_modules.ParameterMap) error {
+func (m *DelugeOutputModule) Configure(
+	params *base_modules.ParameterMap) error {
 	serverParam, ok := (*params)["server"]
 	if !ok || len(serverParam) == 0 {
 		return fmt.Errorf("required server parameter not found")
@@ -60,7 +58,8 @@ func (m *DelugeOutputModule) Parameters() *base_modules.ParameterMap {
 	}
 }
 
-func (m *DelugeOutputModule) Duplicate(specificId string) (base_modules.Module, error) {
+func (m *DelugeOutputModule) Duplicate(specificId string) (base_modules.Module,
+	error) {
 	duplicate := NewDelugeOutputModule(specificId)
 	err := pipeliner_modules.RegisterPipelinerOutputModule(duplicate)
 	if err != nil {
@@ -70,57 +69,15 @@ func (m *DelugeOutputModule) Duplicate(specificId string) (base_modules.Module, 
 	return duplicate, nil
 }
 
-func (m *DelugeOutputModule) GetInputChannel() chan<- *datatypes.PipelineItem {
-	return m.inputChannel
-}
-
-func (m *DelugeOutputModule) Start(waitGroup *sync.WaitGroup) error {
-	if !m.Ready() {
-		waitGroup.Done()
-		return fmt.Errorf("not ready")
-	}
-
-	if m.inputChannel == nil {
-		waitGroup.Done()
-		return fmt.Errorf("input channel not connected")
-	}
-
-	go m.doWork(waitGroup)
-
-	return nil
-}
-
-func (m *DelugeOutputModule) Stop() {
-	close(m.quitChannel)
-	m.quitChannel = make(chan struct{})
-}
-
-func (m *DelugeOutputModule) doWork(waitGroup *sync.WaitGroup) {
-	defer waitGroup.Done()
-L:
-	for {
-		select {
-		case item, ok := <-m.inputChannel:
-			if ok {
-				m.sendToDeluge(item)
-			} else {
-				m.inputChannel = nil
-				break L
-			}
-		case <-m.quitChannel:
-			break L
-		}
-	}
-}
-
-func (m *DelugeOutputModule) sendToDeluge(item *datatypes.PipelineItem) {
+func (m *DelugeOutputModule) sendItemToDeluge(
+	item *datatypes.PipelineItem) bool {
 	// Use first URL available.
 	// TODO(bga): Allow user to define preferred hosts or URL types (e.g.
 	// prefer magnet links).
 	torrentUrl, err := item.GetUrl(0)
 	if err != nil {
 		// TODO(bga): Log error.
-		return
+		return false
 	}
 
 	// TODO(bga): Empty configuration for now.
@@ -136,9 +93,13 @@ func (m *DelugeOutputModule) sendToDeluge(item *datatypes.PipelineItem) {
 			torrentUrl.String(), options)
 	default:
 		// TODO(bga): Add handling of other types.
+		return false
 	}
+
+	return true
 }
 
 func init() {
-	pipeliner_modules.RegisterPipelinerOutputModule(NewDelugeOutputModule(""))
+	pipeliner_modules.RegisterPipelinerOutputModule(
+		NewDelugeOutputModule(""))
 }
